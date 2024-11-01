7bd1df147d0f883e6a532c98a322d7a9c890aa95
emmanuel555 <emmanuel_tang@sutd.edu.sg>
2024-11-01 19:08:47 +0800

    cont python acos

--- Data_process.py
+++ Data_process.py
@@ -26,7 +26,10 @@ class RealTimeProcessor(object):
         self.quat_x = 0
         self.quat_y = 0
         self.quat_z = 0
-        self.quat_w = 0        
+        self.quat_w = 0  
+
+        # tpp angle
+        self.tpp = np.array([0, 0, 0])  # 3 x 1   
 
         # filted data with IIR2Filter
         self.px_filted = 0
@@ -53,6 +56,11 @@ class RealTimeProcessor(object):
         self.FilterY = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
         self.FilterZ = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
 
+        self.FilterQW = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
+        self.FilterQX = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
+        self.FilterQY = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
+        self.FilterQZ = IIR2Filter(order, [cutoff], ftype, design=design, rs=rs, fs=sample_rate)
+
         self.FilterOmega_x = IIR2Filter(5, [50], ftype, design=design, rs=rs, fs=sample_rate)
         self.FilterOmega_y = IIR2Filter(5, [50], ftype, design=design, rs=rs, fs=sample_rate)
         self.FilterOmega_z = IIR2Filter(5, [50], ftype, design=design, rs=rs, fs=sample_rate)
@@ -64,22 +72,50 @@ class RealTimeProcessor(object):
         
     def data_unpack(self, udp_data): ## qns abt this
         x, y, z, qx, qy, qz, qw = struct.unpack("hhhhhhh", udp_data) # h refers to python type integer of byte size 2
-        self.px = x * 0.0005  # position px 
-        self.py = y * 0.0005  # position py  
-        self.pz = z * 0.0005  # position pz 
+        
+        #self.px = x * 0.0005  # position px 
+        #self.py = y * 0.0005  # position py  
+        #self.pz = z * 0.0005  # position pz 
 
-        self.quat_x = float(qx * 0.001)
+        self.px = float(x)   # position px 
+        self.py = float(y)   # position py  
+        self.pz = float(z)   # position pz 
+
+        """ self.quat_x = float(qx * 0.001)
         self.quat_y = float(qy * 0.001)
         self.quat_z = float(qz * 0.001)
-        self.quat_w = float(qw * 0.001)
+        self.quat_w = float(qw * 0.001) """
+
+        self.quat_x = float(qx)
+        self.quat_y = float(qy)
+        self.quat_z = float(qz)
+        self.quat_w = float(qw)
+
         raw_data = [self.px, self.py, self.pz, self.quat_x, self.quat_y, self.quat_z, self.quat_w]
         return raw_data
 
-    def get_data_filted(self):
+    def get_data_filted(self,udp_data):
+        x, y, z, qx, qy, qz, qw = struct.unpack("hhhhhhh", udp_data) # h refers to python type integer of byte size 2
+
+        self.px = float(x)  # position px 
+        self.py = float(y)  # position py  
+        self.pz = float(z)  # position pz 
+
+        self.quat_x = float(qx)
+        self.quat_y = float(qy)
+        self.quat_z = float(qz)
+        self.quat_w = float(qw)
+
         self.px_filted = self.FilterX.filter(self.px)
         self.py_filted = self.FilterY.filter(self.py)
         self.pz_filted = self.FilterZ.filter(self.pz)
-        filted_data = [self.px_filted, self.py_filted, self.pz_filted]
+
+        self.quat_w_filted = self.FilterQW.filter(self.quat_w)
+        self.quat_x_filted = self.FilterQX.filter(self.quat_x)
+        self.quat_y_filted = self.FilterQY.filter(self.quat_y)
+        self.quat_z_filted = self.FilterQZ.filter(self.quat_z)
+
+        filted_data = [self.px_filted, self.py_filted, self.pz_filted, self.quat_w_filted, self.quat_x_filted, self.quat_y_filted, self.quat_z_filted]
 
         return filted_data
 
@@ -109,6 +145,7 @@ class RealTimeProcessor(object):
         rotm = [self.R11, self.R12, self.R13, self.R21, self.R22, self.R23, self.R31, self.R32, self.R33]
 
         return rotm
+        
 
     def get_heading_x(self):
         # # rotation matrix
@@ -141,13 +178,41 @@ class RealTimeProcessor(object):
         roll_x = math.atan2(2*self.quat_w*self.quat_x + 2*self.quat_y*self.quat_z, 1 - 2*(self.quat_y*self.quat_y + self.quat_x*self.quat_x))
         return roll_x
 
-    def get_tpp(self): # qns abt this
-        xi_x = math.atan2(self.R13, self.R33)
+    def get_tpp_angle(self): # qns abt this
+
+        #compute rot matrix
+        xx = self.quat_x * self.quat_x
+        yy = self.quat_y * self.quat_y
+        zz = self.quat_z * self.quat_z
+        xy = self.quat_x * self.quat_y
+        xz = self.quat_x * self.quat_z
+        yz = self.quat_y * self.quat_z
+        wx = self.quat_w * self.quat_x
+        wy = self.quat_w * self.quat_y
+        wz = self.quat_w * self.quat_z
+
+        self.R11 = 1 - 2 * (yy + zz)
+        self.R12 = 2 * (xy - wz)
+        self.R13 = 2 * (xz + wy)
+        self.R21 = 2 * (xy + wz)
+        self.R22 = 1 - 2 * (xx + zz)
+        self.R23 = 2 * (yz - wx)
+        self.R31 = 2 * (xz - wy)
+        self.R32 = 2 * (yz + wx)
+        self.R33 = 1 - 2 * (xx + yy)
+
+        xi_x = math.atan2(self.R13, self.R33) # atan2(opp,adj)
         xi_y = math.atan2(self.R23, self.R33)
+        xi_z = 0
         # xi_x = self.R13
         # xi_y = self.R23
-        tpp = [xi_x, xi_y]
-        return tpp
+        # tpp = [xi_x, xi_y, xi_z] # disk euler angle vector about (x, y, z)
+        
+        self.tpp[0] = xi_x
+        self.tpp[1] = xi_y
+        self.tpp[2] = xi_z
+
+        return self.tpp
 
     def get_z_x(self):
         z_x = [self.R11, self.R21, self.R31]
@@ -225,10 +290,40 @@ class RealTimeProcessor(object):
         self.Omega = 2 * np.dot(E_q_trans, dot_quat) # 3 x 1 - about x, y, z
         self.Omega_dot = 2 * np.dot(E_q_trans, dot_dot_quat) # 3 x 1 - about x, y, z
 
-
         return (self.Omega, self.Omega_dot)
 
     def get_Omega_dot_dotdot_filt(self):
+
+        diff_qx = self.quat_x - self.qx_last
+        diff_qy = self.quat_y - self.qy_last
+        diff_qz = self.quat_z - self.qz_last
+        diff_qw = self.quat_w - self.qw_last
+
+        diff_diff_qx = diff_qx - self.diff_qx_last
+        diff_diff_qy = diff_qy - self.diff_qy_last
+        diff_diff_qz = diff_qz - self.diff_qz_last
+        diff_diff_qw = diff_qw - self.diff_qz_last
+
+        self.qx_last = self.quat_x
+        self.qy_last = self.quat_y
+        self.qz_last = self.quat_z
+        self.qw_last = self.quat_w
+
+        self.diff_qx_last = diff_qx
+        self.diff_qy_last = diff_qy
+        self.diff_qz_last = diff_qz
+        self.diff_qw_last = diff_qw
+
+        dot_quat = [diff_qw/self.sample_time, diff_qx/self.sample_time, diff_qy/self.sample_time, diff_qz/self.sample_time]
+        dot_dot_quat = [diff_diff_qw/self.sample_time, diff_diff_qx/self.sample_time, diff_diff_qy/self.sample_time, diff_diff_qz/self.sample_time]
+
+        E_q_trans = [[-self.quat_x, self.quat_w, self.quat_z, -self.quat_y],
+                     [-self.quat_y, -self.quat_z, self.quat_w, self.quat_x],
+                     [-self.quat_z, self.quat_y, -self.quat_x, self.quat_w]]
+        
+        self.Omega = 2 * np.dot(E_q_trans, dot_quat) # 3 x 1 - about x, y, z
+        self.Omega_dot = 2 * np.dot(E_q_trans, dot_dot_quat) # 3 x 1 - about x, y, z
+        
         Omega_x = self.Omega[0]
         Omega_y = self.Omega[1]
         Omega_z = self.Omega[2]
@@ -245,12 +340,17 @@ class RealTimeProcessor(object):
         self.Omega_dot_y_f = self.FilterOmega_dot_y.filter(Omega_dot_y)
         self.Omega_dot_z_f = self.FilterOmega_dot_z.filter(Omega_dot_z)
 
-        self.Omega_f = [self.Omega_x_f, self.Omega_y_f, self.Omega_z_f]
-        self.Omega_dot_f = [self.Omega_dot_x_f, self.Omega_dot_y_f, self.Omega_dot_z_f]
+        # convention is abt x, y, z - pry
+        #self.Omega_f = [self.Omega_x_f, self.Omega_y_f, self.Omega_z_f]
+        #self.Omega_dot_f = [self.Omega_dot_x_f, self.Omega_dot_y_f, self.Omega_dot_z_f]
 
-        return (self.Omega_f, self.Omega_dot_f) # convention is abt x, y, z - pry
+        # convention is abt y, x, z - rpy
+        self.Omega_f_rpy = [self.Omega_y_f, self.Omega_x_f, self.Omega_z_f]
+        self.Omega_dot_f_rpy = [self.Omega_dot_y_f, self.Omega_dot_x_f, self.Omega_dot_z_f]
 
-    def get_RPY(self): # qns abt this
+        return (self.Omega_f_rpy, self.Omega_dot_f_rpy) # convention is abt y, x, z - rpy world frame
+
+    def get_RPY(self): # solely for quad
         # roll - rotating about x axis
         roll_a = 2 * (self.quat_w * self.quat_x + self.quat_y * self.quat_z)
         roll_b = 1 - 2 * (self.quat_x * self.quat_x + self.quat_y * self.quat_y)
@@ -267,6 +367,55 @@ class RealTimeProcessor(object):
 
         RPY = [angle_roll, angle_pitch, angle_yaw] # pry
         return RPY
+    
+    def tpp_eulerAnglesToQuaternion(self):
+        """
+        Convert an Euler angle to a quaternion.
+        
+        We have used the following definition of Euler angles.
 
+        - Tait-Bryan variant of Euler Angles
+        - Yaw-pitch-roll rotation order (ZYX convention), rotating around the z, y and x axes respectively
+        - Intrinsic rotation (the axes move with each rotation)
+        - Active (otherwise known as alibi) rotation (the point is rotated, not the coordinate system)
+        - Right-handed coordinate system with right-handed rotations
+        
+        Parameters
+        ----------
+        eulerAngles : 
+            [3x1] np.ndarray  
+            [roll, pitch, yaw] angles in radians 
+                
+        Returns
+        -------
+        p : [4x1] np.ndarray
+            quaternion defining a given orientation
+    """
+        """ if isinstance(eulerAngles, list) and len(eulerAngles)==3:
+            eulerAngles = np.array(eulerAngles) 
+        elif isinstance(eulerAngles, np.ndarray) and eulerAngles.size==3:
+            pass
+        else:
+            raise TypeError("The eulerAngles must be given as [3x1] np.ndarray vector or a python list of 3 elements")
+        
+        
+        roll = eulerAngles[0]
+        pitch = eulerAngles[1]
+        yaw = eulerAngles[2]
+        """
+        
+        roll = self.tpp[0]
+        pitch = self.tpp[1]
+        yaw = self.tpp[2]
+        
+        q0 = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        q1 = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        q2 = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        q3 = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        
+        p = np.r_[q0, q1, q2, q3]
+        
+        return p
+    
 
 
--- test_Minsnap.py
+++ test_Minsnap.py
@@ -1,5 +1,9 @@
 import minsnap_trajectories as ms
 import numpy as np
+import numpy.linalg as la
+import math
+from pyquaternion import Quaternion
+from pyrr import Quaternion, Matrix33, Matrix44, Vector3
 
 
 if __name__ == '__main__':
@@ -36,3 +40,23 @@ if __name__ == '__main__':
     # available order in derivatives in pva is one lesser than computed meaning up to one derivative less  
 
     print(pva[5,99,2]) # up to order of derivatives available, waypoints, axis
+
+    # print(np.shape(np.array([[1,2,3],[2,3,1]]))) # size is 2 x 3
+
+    z = np.array([2,3,1]) # 3 x 1
+    z = np.array([[2,3,1]]) # 1 x 3
+
+    z = np.transpose(z)
+    print(np.shape(z))
+
+    a = np.array([[0,0,1]]) # 1 x 3 - row based simulated disk vector 
+    b = np.array([[1,0,1]]) # 1 x 3 - row based simulated zd which is desired vector 
+    c = np.dot(a,np.transpose(b))
+    d = la.norm(a,2)*la.norm(b,2) # L2 norm of a and b
+    angle = math.acos(c/d)
+
+    n = np.cross(a,b)/la.norm(np.cross(a,b)) # cross product of a and b
+    
+    print(np.shape(n))
+
+    #print(math.degrees(angle))
