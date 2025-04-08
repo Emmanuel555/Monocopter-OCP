5c4f4ab57176e38295f7bd3e428b1df786a5b8a6
emmanuel555 <emmanuel_tang@sutd.edu.sg>
2025-04-08 20:01:03 +0800

    update for today

---
+++ CF_cyclic_monoco.py
@@ -0,0 +1,291 @@
+import cflib.crtp
+import pygame
+from cflib.crazyflie import Crazyflie
+from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
+
+import logging
+import time
+
+import Mocap
+import DataSave
+import Data_process
+
+import math
+from pyrr import quaternion
+import numpy as np
+import numpy.linalg as la
+
+import monoco_att_ctrl
+import trajectory_generator
+import timeit
+from pynput import keyboard
+
+from cflib.crazyflie.swarm import CachedCfFactory
+from cflib.crazyflie.swarm import Swarm
+
+from cflib.crazyflie.log import LogConfig
+from cflib.crazyflie.syncLogger import SyncLogger
+
+
+# robot address
+# Change uris and sequences according to your setup
+
+# monoco radio 1
+URI1 = 'radio://0/80/2M/E7E7E7E701'
+
+
+uris = {
+    URI1,
+}
+
+
+def swarm_exe(cmd_att):
+    seq_args = {
+        URI1: [cmd_att[0]],
+    }
+    return seq_args
+
+
+def swarm_logging(swarm_log):
+    seq_args = {
+        URI1: [swarm_log[0]],
+    }
+    return seq_args
+
+
+def init_throttle(scf, cmds):
+    try:
+        cf = scf.cf
+        cf.param.set_value('kalman.resetEstimation', '1')
+        time.sleep(0.1)
+        cf.param.set_value('kalman.resetEstimation', '0')
+        time.sleep(0.1)
+
+        # initialise the controller
+        cf.param.add_update_callback(group='stabilizer', name='estimator',
+                                 cb=param_stab_est_callback)
+        cf.param.set_value('stabilizer.controller', '3')  # 3: INDI controller
+        time.sleep(0.1)
+
+        #cf.commander.send_setpoint(int(cmds[0]), int(cmds[1]), int(cmds[2]), int(cmds[3])) # roll, pitch, yawrate, thrust 
+        cf.commander.send_position_setpoint(int(cmds[0]), int(cmds[1]), int(cmds[2]), int(cmds[3])) 
+        print("Initialisation monoco....set ur sticks to mid and down")
+        time.sleep(0.1)
+    except Exception as e:
+        print("Initialisation error: ", e)
+
+
+def arm_throttle(scf, cmds):
+    try:
+        cf = scf.cf
+        cf.param.set_value('stabilizer.controller', '3')  # 3: INDI controller
+        cf.commander.send_position_setpoint(int(cmds[0]), int(cmds[1]), int(cmds[2]), int(cmds[3]))  
+        #print('arming w thrust val....', cmds[3])
+    except Exception as e:
+        print("swarming error: ", e)
+
+
+def transmitter_calibration():
+    # where hand control comes
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    lowest = 0.97
+    highest = -1
+    range_js = -(highest - lowest)
+    range_motor = 65535
+    rate = range_motor / range_js
+    a0 = joystick.get_axis(0)  # x axis right hand <- ->
+    a1 = joystick.get_axis(1)  # y axis right hand up down
+    a2 = joystick.get_axis(2)  # thrust
+    a3 = joystick.get_axis(3)
+
+    button0 = joystick.get_button(0)
+    button1 = joystick.get_button(1)
+
+    # thrust from control pad
+    conPad = int((a2 - highest) * rate)
+
+    # throttle joystick saturation
+    if conPad < 10:
+        conPad = 10
+    if conPad > 65500:
+        conPad = 65500
+
+    # takeoff sign
+    if conPad < 2000:
+        enable = 0
+    else:
+        enable = 1
+
+    cmd = conPad*enable
+    return (cmd, button0, button1, a0, a1, a2, a3)
+
+
+def logging_config():
+    # Create the log config for the position
+    lg_stab = LogConfig(name='gyro_tracking', period_in_ms=10) # limited to 100 hz, period = 10/1000
+    lg_stab.add_variable('gyro.x', 'float') # deg/s
+    lg_stab.add_variable('gyro.y', 'float')
+    lg_stab.add_variable('gyro.z', 'float')
+    lg_stab.add_variable('ctrlINDI.Omega_f_p', 'float') # rad/s
+    lg_stab.add_variable('ctrlINDI.Omega_f_q', 'float')
+    lg_stab.add_variable('ctrlINDI.Omega_f_r', 'float')
+    # lg_stab.add_variable('ctrlINDI.rate_d[0]dd', 'float') # rad/s^2
+    # lg_stab.add_variable('ctrlINDI.rate_d[1]', 'float')
+    # lg_stab.add_variable('ctrlINDI.rate_d[2]', 'float')
+
+    return (lg_stab)
+
+
+def param_stab_est_callback(name, value):
+    print('The crazyflie monocopter has parameter ' + name + ' set at number: ' + value)
+
+
+def log_stab_callback(timestamp, data, logconf):
+    #print('[%d][%s]: %s' % (timestamp, logconf.name, data))
+    gyro_x = data.get('gyro.x') # roll float
+    gyro_y = data.get('gyro.y') # pitch
+    gyro_z = data.get('gyro.z') # yaw
+    omega_roll = data.get('ctrlINDI.Omega_f_p') # r rad/s
+    omega_pitch = data.get('ctrlINDI.Omega_f_q') # p
+    omega_yaw = data.get('ctrlINDI.Omega_f_r') # y
+    # omega_roll_dot = data.get('ctrlINDI.rate_d[0]dd', 'float') # r rad/s^2
+    # omega_pitch_dot = data.get('ctrlINDI.rate_d[1]', 'float') # p
+    # omega_yaw_dot = data.get('ctrlINDI.rate_d[2]', 'float') # y
+    print('omega_roll:', omega_roll)
+
+
+def log_async(scf, logconf):
+    cf = scf.cf
+    cf.log.add_config(logconf)
+    logconf.data_received_cb.add_callback(log_stab_callback)
+    logconf.start()
+    #time.sleep(5)
+    #logconf.stop()
+
+
+if __name__ == '__main__':
+
+    # Setup keyboard listener
+    stage = 'hover'
+    # def on_press(key):
+    #     global stage
+    #     try:
+    #         if key.char == 'g':  # Check if 'g' is pressed
+    #             stage = 'trajectory on'
+    #     except AttributeError:
+    #         pass  # Ignore special keys
+
+    # # Start listening for key presses
+    # listener = keyboard.Listener(on_press=on_press)
+    # listener.start()
+
+    """ data_receiver_sender = Mocap.Udp()
+    max_sample_rate = 360
+    sample_rate = data_receiver_sender.get_sample_rate()
+    sample_time = 1 / sample_rate
+    data_processor = Data_process.RealTimeProcessor(5, [64], 'lowpass', 'cheby2', 85, sample_rate)
+
+    data_saver = DataSave.SaveData('Data_time',
+                                   'Monocopter_XYZ','ref_position','rmse_num_xyz','final_rmse','ref_msg','status','cmd','tpp_angle','tpp_omega','tpp_omega_dot','velocity','z_control','des_thrust','ref_rates','ref_raterates','precession_rate','yawrate')
+          """             
+                                   
+    logging.basicConfig(level=logging.ERROR)
+    cflib.crtp.init_drivers()
+
+    # Initialize the joysticks
+    pygame.init()
+    pygame.joystick.init()
+    done = False
+    controllerEnable = False
+    pad_speed = 1
+
+    # Status
+    status = "pending for state estimation"
+    
+    
+    time_last = 0
+    count = 0        
+    time_start = time.time()
+    minutes = 1 # no.of mins to run this loop
+    time_end = time.time() + (60*100*minutes) 
+    
+
+    with Swarm(uris, factory= CachedCfFactory(rw_cache='./cache')) as swarm:
+        #swarm.reset_estimators()
+        cmd_att_startup = np.array([0, 0, 0, 0]) # init setpt to 0 0 0 0
+        cmd_att = np.array([cmd_att_startup])
+        data_log = logging_config()
+        swarm_log = np.array([data_log])
+        seq_args_log = swarm_logging(swarm_log)
+        seq_args = swarm_exe(cmd_att)
+        swarm.parallel(init_throttle, args_dict=seq_args)
+        swarm.parallel(log_async, args_dict=seq_args_log)
+
+        try:
+            #while time_end > time.time():
+            while True:
+                start = timeit.default_timer() 
+                abs_time = time.time() - time_start
+
+                """ # require data from Mocap
+                data = data_receiver_sender.get_data()
+
+                # data unpack
+                data_processor.data_unpack_filtered(data)
+
+                # processed tpp data/feedback
+                # rotational_state_vector = data_processor.get_Omega_dot_dotdot_filt_eul_finite_diff()
+                rotational_state_vector = data_processor.get_Omega_dot_dotdot_filt_eul_central_diff()
+                linear_state_vector = data_processor.pos_vel_acc_filtered()
+                body_pitch = data_processor.body_pitch
+                tpp_angle = data_processor.tpp
+                tpp_omega = data_processor.Omega
+                tpp_omega_dot = data_processor.Omega_dot
+                body_yaw = data_processor.yaw
+                yawrate = data_processor.get_yawrate()
+                # tpp_omega = data_processor.Omega
+                # tpp_omega_dot = data_processor.Omega_dot
+                tpp_quat = data_processor.tpp_eulerAnglesToQuaternion() """
+
+                # time difference needed to calculate velocity
+                dt = time.time() - time_last  #  time step/period
+                time_last = time.time()
+
+                tx_cmds = transmitter_calibration()  # get the joystick commands
+                cmd = tx_cmds[0]  # thrust command
+                button0 = tx_cmds[1]
+                button1 = tx_cmds[2]
+                a0 = tx_cmds[3]     
+                a1 = tx_cmds[4]
+                a2 = tx_cmds[5]
+                a3 = tx_cmds[6]
+
+                # control input (traj execution)
+                final_cmd = np.array([cmd, cmd, cmd, cmd]) 
+                final_cmd = np.array([final_cmd])
+                seq_args = swarm_exe(final_cmd)
+                swarm.parallel(arm_throttle, args_dict=seq_args)
+
+                if count % 10 == 0:
+                    print('tx commands: ', cmd, button0, button1, a0, a1, a2, a3)
+
+                    if dt > 0.0:
+                        print('frequency (Hz) = ', 1/dt)
+                        #print('time step: ', dt, 'abs time: ', abs_time)
+
+                count += 1
+
+
+        except KeyboardInterrupt:    
+            print('tx commands: ', cmd, button0, button1, a0, a1, a2, a3) 
+                    
+
+# save data
+#path = '/home/emmanuel/AFC_Optitrack/robot_solo/'
+#data_saver.save_data(path)
+
